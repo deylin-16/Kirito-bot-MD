a726542b43159c4d6c6450ddbf4950fefbebe47d
deylin-16 <deylin1616@gmail.com>
2025-12-24 23:16:18 -0600

    Actualizar ©prueba.js

--- plugins/©prueba.js
+++ plugins/©prueba.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
-    const config = global.getAssistantConfig(conn.user.jid)
+    const config = 'https://raw.githubusercontent.com/deylin-16/Kirito.my/refs/heads/main/media/images/2.jpg?token=GHSAT0AAAAAADQVEIPPZ4NQNHC2T45Q7HQU2KMZCHQ'
     
-    let isBuffer = Buffer.isBuffer(config.assistantImage)
+    let isBuffer = Buffer.isBuffer(config)
     let targetUrl = 'https://www.deylin.xyz'
 
     await conn.sendMessage(m.chat, {
