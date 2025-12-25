5607416579bb039e4de42d50eaaa9261d7aac514
deylin-16 <deylin1616@gmail.com>
2025-12-25 11:24:23 -0600

    Crear ©assistant-chatgpt.js

---
+++ plugins/©assistant-chatgpt.js
@@ -0,0 +1,27 @@
+import fetch from "node-fetch";
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) {
+    return conn.reply(m.chat, ` ¡Hola! ¿cómo puedo ayudarte hoy?`, m);
+  }
+    await m.react('✨')
+
+  try {
+    const url = `${url_api}/api/ia/texto/chatgpt?q=${encodeURIComponent(text)}&apikey=by_deylin`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (!data || !data.response) {
+      return conn.reply(m.chat, "❌ No recibí respuesta de la IA, intenta de nuevo.", m);
+    }
+    await m.react('🌟')
+    await conn.reply(m.chat, `${data.response}`, m);
+  } catch (e) {
+    console.error(e);
+    await conn.reply(m.chat, "⚠️ Hubo un error al conectar con la IA.", m);
+  }
+};
+
+handler.command =['ia']
+
+export default handler;
