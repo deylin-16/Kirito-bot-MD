239c20e51f7479f7d0e63f33398f50b69fff11d8
deylin-16 <deylin1616@gmail.com>
2025-12-25 13:05:14 -0600

    Actualizar ©prueba.js

--- plugins/©prueba.js
+++ plugins/©prueba.js
@@ -1,31 +1,14 @@
 let handler = async (m, { conn }) => {
     const config = global.getAssistantConfig(conn.user.jid)
-    
-    // Configuración de variables
-    let redes = 'https://www.deylin.xyz/1' 
+    let redes = 'https://www.deylin.xyz/1'
     let icono = 'https://i.ibb.co/g8PsK57/IMG-20251224-WA0617.jpg'
     let textbot = `Asistente: ${config.assistantName}`
 
     await conn.sendMessage(m.chat, {
-        text: redes, 
-        contextInfo: { 
-            isForwarded: true, 
-            externalAdReply: { 
-                showAdAttribution: true, 
-                title: textbot, 
-                body: '🚀 ♡⃝𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕҉ᚐ', 
-                mediaUrl: null, 
-                description: null, 
-                previewType: "PHOTO", 
-                thumbnailUrl: icono, 
-                sourceUrl: redes, 
-                mediaType: 1, 
-                renderLargerThumbnail: false 
-            } 
-        }
+        image: { url: icono },
+        caption: `\( {textbot}\n🚀 ♡⃝𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕҉ᚐ\n\n \){redes}`
     }, { quoted: m })
 }
 
 handler.command = ['prueba']
-
 export default handler
