9915766681c67b5ff7b8dfe5409966159d3c6768
deylin-16 <deylin1616@gmail.com>
2025-12-26 12:23:31 -0600

    Actualizar ©prueba.js

--- plugins/©prueba.js
+++ plugins/©prueba.js
@@ -1,38 +1,15 @@
-import ImageKit from "imagekit"
-import fetch from "node-fetch"
-
-const imagekit = new ImageKit({
-    publicKey: "public_UilqC3N3XUQp2rRJcGGhLhaXKSY=",
-    privateKey: "private_ojSXwbW+qGniUaMFMzzVNWhiuI8=",
-    urlEndpoint: "https://ik.imagekit.io/pm10ywrf6f"
-})
-
-let handler = async (m, { conn, command }) => {
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (!mime) return m.reply(`Responde a una imagen o video con el comando *${command}*`)
-    
-    await m.reply('Procesando y subiendo archivo...')
-    
-    let media = await q.download()
-    let fileName = `${Date.now()}.${mime.split('/')[1]}`
-
-    imagekit.upload({
-        file: media,
-        fileName: fileName,
-        folder: "/bot_uploads"
-    }, async (err, result) => {
-        if (err) return m.reply('Error al subir: ' + err.message)
-        
-        let txt = `*ARCHIVO SUBIDO EXITOSAMENTE*\n\n`
-        txt += `*ID:* ${result.fileId}\n`
-        txt += `*Nombre:* ${result.name}\n`
-        txt += `*URL:* ${result.url}\n`
-        txt += `*Tipo:* ${result.fileType}`
-
-        await conn.sendMessage(m.chat, { text: txt }, { quoted: m })
-    })
+let handler = async (m, { conn }) => {
+    try {
+        await conn.sendModify(m.chat, "332", m, {
+            title: 'PRUEBA DE DISEÑO',
+            body: 'Si ves esto, la función funciona',
+            url: "https://www.deylin.xyz",
+            thumbnail: await (await fetch("https://i.ibb.co/g8PsK57/IMG-20251224-WA0617.jpg")).buffer(),
+            largeThumb: true
+        });
+    } catch (e) {
+        m.reply("❌ Tu bot no tiene implementada la función 'sendModify' en lib/simple.js");
+    }
 }
-
-handler.command = ['increase']
+handler.command = ['testdesign']
 export default handler
