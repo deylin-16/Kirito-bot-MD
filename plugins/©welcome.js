0908a9c300522111ceb582ffeb02a83d0290a7bf
deylin-16 <deylin1616@gmail.com>
2025-12-25 19:20:12 -0600

    Actualizar ©welcome.js

--- plugins/©welcome.js
+++ plugins/©welcome.js
@@ -1,17 +1,20 @@
 import { WAMessageStubType } from '@whiskeysockets/baileys'
 
 export async function before(m, { conn, participants, groupMetadata }) {
+    
     if (!m.messageStubType || !m.isGroup) return true
 
     const botSettings = global.db.data.settings[conn.user.jid] || {}
     const mainBotJid = global.conn?.user?.jid
     const currentBotJid = conn.user.jid
 
+    
     if (currentBotJid === mainBotJid && botSettings.soloParaJid) return true
 
     const chat = global.db.data.chats[m.chat]
     if (!chat?.welcome) return true
 
+    
     const isAdd = m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD || m.messageStubType === WAMessageStubType.GROUP_CHANGE_MEMBERS
     if (!isAdd) return true
 
@@ -22,6 +25,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
     const nombreDelGrupo = groupMetadata.subject
     const mentionListText = `@${who.split('@')[0]}`
 
+    
     let ppUrl = 'https://i.ibb.co/jPSF32Pz/9005bfa156f1f56fb2ac661101d748a5.jpg'
     try {
         ppUrl = await conn.profilePictureUrl(who, 'image')
@@ -39,15 +43,18 @@ export async function before(m, { conn, participants, groupMetadata }) {
         .replace(/@grupo/g, nombreDelGrupo)
         .replace(/@total/g, totalMembers)
 
+    
     let fkontak = {
         key: { fromMe: false, participant: "0@s.whatsapp.net", remoteJid: "status@broadcast" },
         message: { contactMessage: { displayName: `BIENVENID@ A ${nombreDelGrupo}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;Admin;;;\nFN:Admin\nEND:VCARD` } }
     }
 
     try {
+        
         if (typeof global.design === 'function' && currentBotJid !== mainBotJid) {
             await global.design(conn, m, finalCaption)
         } else {
+            
             await conn.sendMessage(m.chat, {
                 image: { url: ppUrl },
                 caption: finalCaption.trim(),
@@ -55,7 +62,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
             }, { quoted: fkontak })
         }
     } catch (e) {
-        console.error('Error en Welcome:', e)
+        console.error('Error al enviar bienvenida:', e)
     }
 
     return true
